d4158b17a3e45cfb92de9433a8f41c920db5403b
test <dangle316@gmail.com>
2019-07-06 22:55:13 -0400

    fix test

--- csvParserFull.sh
+++ csvParserFull.sh
@@ -2,9 +2,9 @@
 # setupapachevhost.sh - Apache webhosting automation demo script
 file=sampleCSV.csv
 
-WHITE= "\e[0m"
-RED= "\e[31m"
-EOR = "|THISISTHEENDOFTHEREQUEST|"
+WHITE="\e[0m"
+RED="\e[31m"
+EOR="|THISISTHEENDOFTHEREQUEST|"
 
 
 # set the Internal Field Separator to |
@@ -16,7 +16,9 @@ do
 	printf "%b Checking %s... " $COLOR $url
 	
 	# Run Curl Command
-	RESPONSE=$(curl -s -o /dev/null -w $EOR'%{http_code} - %{time_total}' $url)
+	RESPONSE=$(curl -s -w $EOR'%{http_code} - %{time_total}' $url)
+	
+	# Parse Status From Respons - https://superuser.com/questions/1001973/bash-find-string-index-position-of-substring
 	STATUS=${RESPONSE#*$EOR}
 
 	# Determine Color from Status Code
