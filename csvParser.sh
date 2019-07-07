09b515524fc2dee3b8118ba06cda74d5cd967af0
test <dangle316@gmail.com>
2019-07-06 22:46:32 -0400

    parsing response

--- csvParser.sh
+++ csvParser.sh
@@ -2,13 +2,30 @@
 # setupapachevhost.sh - Apache webhosting automation demo script
 file=sampleCSV.csv
 
+WHITE= "\e[0m"
+RED= "\e[31m"
+
+
 # set the Internal Field Separator to |
 IFS='|'
 while read -r url ip
 do
-        printf "Checking %s... " $url
-		STATUS=$(curl -s -o /dev/null -w '%{http_code} - %{time_total}' $url)
-		printf "Status: %s!\n" $STATUS
+	COLOR=$WHITE
 
+	printf "%b Checking %s... " $COLOR $url
 	
+	# Run Curl Command
+	STATUS=$(curl -s -o /dev/null -w '%{http_code} - %{time_total}' $url)
+
+	# Determine Color from Status Code
+	case $STATUS in
+		*"200 -"*)
+			COLOR=$WHITE;;
+		*)
+			COLOR=$RED;;
+	esac
+
+	# Output Results		
+	printf "%b Status: %s!\n" $COLOR $STATUS 
+
 done < "$file"
--- csvParserFull.sh
+++ csvParserFull.sh
@@ -2,26 +2,33 @@
 # setupapachevhost.sh - Apache webhosting automation demo script
 file=sampleCSV.csv
 
-WHITE = "\e[0m"
-RED = "\e[31m"
+WHITE= "\e[0m"
+RED= "\e[31m"
+EOR = "|THISISTHEENDOFTHEREQUEST|"
 
 
 # set the Internal Field Separator to |
 IFS='|'
 while read -r url ip
 do
-	COLOR = $WHITE
+	COLOR=$WHITE
 
-    printf "%b Checking %s... " $COLOR $url
+	printf "%b Checking %s... " $COLOR $url
+	
+	# Run Curl Command
+	RESPONSE=$(curl -s -o /dev/null -w $EOR'%{http_code} - %{time_total}' $url)
+	STATUS=${RESPONSE#*$EOR}
 
-	STATUS=$(curl -s -o /dev/null -w '%{http_code} - %{time_total}' $url)
-
+	# Determine Color from Status Code
 	case $STATUS in
 		*"200 -"*)
 			COLOR=$WHITE;;
 		*)
 			COLOR=$RED;;
 	esac
-		
+
+	# Output Results		
 	printf "%b Status: %s!\n" $COLOR $STATUS 
+
+
 done < "$file"
